9c5faecaa98049b920da8dc66dbc21cfddbe82fa
semako123 <zosusemenoch@gmail.com>
2023-08-01 03:59:18 +0100

    Adds dummy data

---
+++ src/data/nfts.js
@@ -0,0 +1,15 @@
+import { NFTspacew } from "../assets";
+import { Avatar1 } from "../assets";
+
+const nftData = [
+	{
+		name: "Space Walking",
+		img: NFTspacew,
+		artist: {
+			name: "AnimaKid",
+			img: Avatar1,
+		},
+	},
+];
+
+export default nftData;
