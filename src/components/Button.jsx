9a94e1baa5020ac4fcf808b0b2f072c4bd8bca5b
semako123 <zosusemenoch@gmail.com>
2023-08-01 18:33:42 +0100

    Fix bug in outlined button prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ outlined, children, lg }) => {
 	return (
 		<div
-			className={`${outlined ? "border-[#A259FF]" : "bg-[#A259FF]"} ${
+			className={`${outlined ? "border-[#A259FF] border-2" : "bg-[#A259FF]"} ${
 				lg ? "px-[40px]" : "px-[25px]"
 			} py-[15px] hover:cursor-pointer text-base text-white rounded-[20px] flex gap-3 w-fit`}>
 			{children}
