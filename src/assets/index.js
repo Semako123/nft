734912adb340daac6352dd858006853f28471b16
semako123 <zosusemenoch@gmail.com>
2023-08-01 03:02:53 +0100

    Adds images to assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export { default as logo } from "./logo.svg";
 export { default as Storefront } from "./Storefront.svg";
 export { default as User } from "./User.svg";
+export { default as Rocket } from "./Rocket.svg";
