d37a1461a59b55fdc34359c41b2e35a49e8e9dd6
semako123 <zosusemenoch@gmail.com>
2023-08-01 02:55:56 +0100

    init containers

---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+export { default as Landing } from "./Landing";
+export { default as Categories } from "./Categories";
+export { default as Collections } from "./Collections";
+export { default as Discover } from "./Discover";
+export { default as Footer } from "./Footer";
+export { default as HIW } from "./HIW";
+export { default as Mailing } from "./Mailing";
+export { default as TopCreators } from "./TopCreators";
