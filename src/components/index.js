7c2306161831540a7a74a72db16ec803f811028e
semako123 <zosusemenoch@gmail.com>
2023-08-01 17:47:51 +0100

    Adds collection component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Button } from "./Button";
 export { default as Info } from "./Info";
 export { default as NftCard } from "./NftCard";
+export { default as Collection } from "./Collection";
