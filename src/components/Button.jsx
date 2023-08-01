c28d704e07dc0669bc5b15233287c21e2e98d882
semako123 <zosusemenoch@gmail.com>
2023-08-01 02:44:19 +0100

    Adds styles to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Button = ({ variant, children, sz }) => {
 	return (
 		<div
-			className={`${variant === "outlined" ? "" : ""} ${
-				sz === "lg" ? "px-[50px]" : "px-[30px]"
-			} py-[20px]`}>
+			className={`${
+				variant === "outlined" ? "border-[#A259FF]" : "bg-[#A259FF]"
+			} ${
+				sz === "lg" ? "px-[40px]" : "px-[25px]"
+				} py-[15px] hover:cursor-pointer text-base text-white rounded-[20px] flex gap-3`}>
 			{children}
 		</div>
 	);
