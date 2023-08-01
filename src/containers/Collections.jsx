e97d115f166abfc6915bb12864da0ebeae8df01f
semako123 <zosusemenoch@gmail.com>
2023-08-01 18:22:34 +0100

    Updates collections -Adds heading/subheading

--- src/containers/Collections.jsx
+++ src/containers/Collections.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Collection } from "../components";
+import { nftCollection } from "../data/nfts";
 
 const Collections = () => {
-  return (
-    <div>Collections</div>
-  )
-}
+	return (
+		<div className="px-20 py-12 text-white">
+			<h3 className="font-semibold text-4xl mb-2">Trending Collections</h3>
+			<p className="text-base mb-14 font-light">
+				Checkout Our Weekly Updated Trending Collections
+			</p>
+			<div className="flex justify-between">
+				{nftCollection.map((data) => (
+					<Collection nftColData={data} />
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default Collections
+export default Collections;
