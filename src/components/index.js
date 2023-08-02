ce425c86174aa511982b7d5a549816bf7ce68b09
semako123 <zosusemenoch@gmail.com>
2023-08-02 01:24:52 +0100

    Init category component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Info } from "./Info";
 export { default as NftCard } from "./NftCard";
 export { default as Collection } from "./Collection";
 export { default as Creator } from "./Creator";
+export { default as Categories } from "./Category";
