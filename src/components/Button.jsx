4eb2604f5dea5884634a81cccb73106de224710b
semako123 <zosusemenoch@gmail.com>
2023-08-02 22:08:11 +0100

    Adds light variant to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = ({ outlined, children, lg }) => {
+const Button = ({ outlined, children, lg, light }) => {
 	return (
 		<div
-			className={`${outlined ? "border-[#A259FF] border-2" : "bg-[#A259FF]"} ${
+			className={`${outlined ? "border-[#A259FF] border-2" : (light ? "bg-white" : "bg-[#A259FF]")} ${
 				lg ? "px-[40px]" : "px-[25px]"
-			} py-[15px] hover:cursor-pointer text-base text-white rounded-[20px] flex gap-3 w-fit`}>
+			} ${light ? "text-black font-semibold" : "text-white"} py-[15px] hover:cursor-pointer text-base  rounded-[20px] flex gap-3 w-fit`}>
 			{children}
 		</div>
 	);
