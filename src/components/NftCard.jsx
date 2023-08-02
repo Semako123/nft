e89e2fb44a98ac63f615c79dbc317ef3852802b9
semako123 <zosusemenoch@gmail.com>
2023-08-02 19:02:40 +0100

    Adds price and highest bid to nft cards

--- src/components/NftCard.jsx
+++ src/components/NftCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NftCard = ({ nft, large }) => {
-	const { name, img, artist } = nft;
+	const { name, img, artist, price, highestBid } = nft;
 	return (
 		<div
 			className={`${
@@ -9,12 +9,28 @@ const NftCard = ({ nft, large }) => {
 			} w-[330px] rounded-2xl overflow-hidden text-white bg-[#3B3B3B]`}>
 			<img src={img} alt={name} className="w-full" />
 			<div>
-				<div className="px-4 py-4">
+				<div className="px-6 py-4">
 					<h5 className="text-base mb-1 font-semibold">{name}</h5>
-					<div className="flex gap-3 items-center">
+					<div className="flex gap-3 items-center mb-5">
 						<img src={artist.img} alt="" className="rounded-full h-5 w-5" />
 						<p className="font-light text-sm">{artist.name}</p>
 					</div>
+					{!large && (
+						<div className="flex justify-between">
+							<div>
+								<p className="text-light text-[#858584] mb-1  font-secondary">
+									Price
+								</p>
+								<p className="font-secondary">{price} ETH</p>
+							</div>
+							<div className="text-right">
+								<p className="text-light text-[#858584] mb-1  font-secondary">
+									Highest Bid
+								</p>
+								<p className=" font-secondary">{highestBid} ETH</p>
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
