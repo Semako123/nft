780af69fc8439e7cce54637109032944c3f84927
semako123 <zosusemenoch@gmail.com>
2023-08-02 02:23:37 +0100

    Updates category card

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Category = () => {
-	return <div>Category</div>;
+const Category = ({ name, img, icon }) => {
+	return (
+		<div className="w-[240px] h-[316px] rounded-[20px] overflow-hidden hover:cursor-pointer">
+			<div className="relative">
+				<img src={img} alt="" className="h-full" />
+				<div className="h-full w-full backdrop-blur-sm absolute top-0 grid place-items-center">
+					<img src={icon} alt="" />
+				</div>
+			</div>
+			<div className="pt-[20px] pb-[25px] px-[30px] bg-[#3B3B3B]">
+				<h5 className="text-xl">{name}</h5>
+			</div>
+		</div>
+	);
 };
 
 export default Category;
