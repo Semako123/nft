b7b6410eee90abf1c3b3d2f18227d346fa2fa0fd
semako123 <zosusemenoch@gmail.com>
2023-08-02 02:24:48 +0100

    imports catrgories data to map into cat cards

--- src/containers/Categories.jsx
+++ src/containers/Categories.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Category } from "../components";
+import { nftCategories } from "../data/nfts";
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
-}
+	return (
+		<div className="px-20 py-12 text-white">
+			<h3 className="font-semibold text-4xl mb-14">Browse Categories</h3>
+			<div className="flex flex-wrap justify-between gap-y-10">
+				{nftCategories.map(({ name, icon, image }) => (
+					<Category name={name} img={image} icon={icon} />
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default Categories
+export default Categories;
