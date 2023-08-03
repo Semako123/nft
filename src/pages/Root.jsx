5811ee38137ba633a4335a1141b10e1b229c0520
semako123 <zosusemenoch@gmail.com>
2023-08-03 22:28:08 +0100

    Adds footer section

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components";
+import { Footer } from "../containers";
 
 const Root = () => {
 	return (
-		<div className="container mx-auto">
+		<div className="mx-auto">
 			<Navbar />
 			<Outlet />
+			<Footer />
 		</div>
 	);
 };
