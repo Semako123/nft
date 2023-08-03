3b8cf46aa07e3503b38e91e07935a9815d649674
semako123 <zosusemenoch@gmail.com>
2023-08-03 22:18:21 +0100

    Fixes formatting

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,15 @@ import React from "react";
 const Button = ({ outlined, children, lg, light }) => {
 	return (
 		<div
-			className={`${outlined ? "border-[#A259FF] border-2" : (light ? "bg-white" : "bg-[#A259FF]")} ${
-				lg ? "px-[40px]" : "px-[25px]"
-			} ${light ? "text-black font-semibold" : "text-white"} py-[15px] hover:cursor-pointer text-base  rounded-[20px] flex gap-3 w-fit`}>
+			className={`${
+				outlined
+					? "border-[#A259FF] border-2"
+					: light
+					? "bg-white"
+					: "bg-[#A259FF]"
+			} ${lg ? "px-[40px]" : "px-[25px]"} ${
+				light ? "text-black font-semibold" : "text-white"
+			} py-[15px] hover:cursor-pointer text-base  rounded-[20px] flex gap-3 w-fit`}>
 			{children}
 		</div>
 	);
