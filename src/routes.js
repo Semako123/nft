8e019335302511ad64a871ed90518e18d0ff669b
semako123 <zosusemenoch@gmail.com>
2023-08-04 17:15:02 +0100

    Adds wallet and auth path to routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {
 	Route,
 	createRoutesFromElements,
 } from "react-router-dom";
-import { Home, Root } from "./pages";
+import { ConnectWallet, Home, Root, Signup } from "./pages";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Root />}>
 			<Route index element={<Home />} />
+			<Route path="wallet" element={<ConnectWallet />} />
+			<Route path="auth" element={<Signup />} />
 		</Route>
 	)
 );
