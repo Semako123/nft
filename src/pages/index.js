576bf868182cd18df3d2863dfdb8d972d44c57b7
semako123 <zosusemenoch@gmail.com>
2023-08-05 23:44:56 +0100

    Exports marketplace page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Root } from "./Root";
 export { default as ConnectWallet } from "./ConnectWallet";
 export { default as Signup } from "./Signup";
 export { default as Ranking } from "./Ranking";
+export { default as Marketplace } from "./Marketplace";
