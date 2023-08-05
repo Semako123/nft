0fc7244eda1ecf1156f1da282f1da2af2c722af7
semako123 <zosusemenoch@gmail.com>
2023-08-05 04:08:26 +0100

    Fixes outline bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const Button = ({ outlined, children, light, lg, dark }) => {
 	return (
+		
 		<div
+			style={{borderWidth:`${outlined}px`}}
 			className={`${
-				outlined ? `border-[#A259FF] border-[${outlined}px]` : " bg-[#A259FF]"
+				outlined ? `border-[#A259FF]` : " bg-[#A259FF]"
 			} ${
 				light
 					? "text-black font-semibold bg-white"
 					: dark
 					? "bg-[#3B3B3B] text-white"
 					: "text-white"
-			} ${lg ? "rounded-full w-full":""} py-[15px] hover:cursor-pointer text-base items-center justify-center rounded-[20px] flex gap-3 w-fit px-[25px]`}>
+				} ${lg ? "rounded-full w-full" : ""} py-[15px] hover:cursor-pointer text-base items-center justify-center rounded-[20px] flex gap-3 w-fit px-[25px]`}>
 			{children}
 		</div>
 	);
