e7284a782ae291fd409c4d9476d3f6c1ffa38e0d
semako123 <zosusemenoch@gmail.com>
2023-08-06 17:39:38 +0100

    Adds nftmarket and colmarket to default exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { default as ConnectWallet } from "./ConnectWallet";
 export { default as Signup } from "./Signup";
 export { default as Ranking } from "./Ranking";
 export { default as Marketplace } from "./Marketplace";
+export { default as NftMarket } from "./NftMarket";
+export { default as CollectionsMarket } from "./CollectionsMarket";
