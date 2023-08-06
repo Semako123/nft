eefbd41b1a88d13f50f79469dc3983a88130906a
semako123 <zosusemenoch@gmail.com>
2023-08-06 17:35:46 +0100

    Adds scroll to top component to root

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components";
 import { Footer } from "../containers";
+import ScrollToTop from "../utils/ScrollToTop";
 
 const Root = () => {
 	return (
 		<div className="mx-auto">
+			<ScrollToTop />
 			<Navbar />
 			<Outlet />
 			<Footer />
