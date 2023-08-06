587f9c1c0ca3f48cdcf7cd4a99247261f00bc6dc
semako123 <zosusemenoch@gmail.com>
2023-08-06 21:03:15 +0100

    Inits exhibition page

---
+++ src/pages/ExhibitionPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExhibitionPage = () => {
+  return (
+    <div>ExhibitionPage</div>
+  )
+}
+
+export default ExhibitionPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,4 @@ export { default as Ranking } from "./Ranking";
 export { default as Marketplace } from "./Marketplace";
 export { default as NftMarket } from "./NftMarket";
 export { default as CollectionsMarket } from "./CollectionsMarket";
+export { default as ExhibitionPage } from "./ExhibitionPage";
