3bbfcaa1e3b1d2da69303c1372a5dffab60059d1
semako123 <zosusemenoch@gmail.com>
2023-08-06 17:59:26 +0100

    Adds See more button to collection / Adds index to mapping

--- src/containers/Collections.jsx
+++ src/containers/Collections.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { Collection } from "../components";
+import { Collection, Button } from "../components";
 import { nftCollection } from "../data/nfts";
+import { Eye } from "../assets";
+import { Link } from "react-router-dom";
 
 const Collections = () => {
 	return (
 		<div className="px-20 py-12 text-white">
-			<h3 className="font-semibold text-4xl mb-2">Trending Collections</h3>
-			<p className="text-base mb-14 font-light">
-				Checkout Our Weekly Updated Trending Collections
-			</p>
+			<div className="flex justify-between mb-[20px]">
+				<div>
+					<h3 className="font-semibold text-4xl mb-2">Trending Collections</h3>
+					<p className="text-base mb-14 font-light">
+						Checkout Our Weekly Updated Trending Collections
+					</p>
+				</div>
+				<div>
+					<Link to="/marketplace/collections">
+						<Button outlined={2}>
+							<img src={Eye} alt="" />
+							<p>See All</p>
+						</Button>
+					</Link>
+				</div>
+			</div>
+
 			<div className="flex justify-between">
-				{nftCollection.map((data) => (
-					<Collection nftColData={data} />
+				{nftCollection.slice(0, 3).map((data, index) => (
+					<Collection nftColData={data} key={index}/>
 				))}
 			</div>
 		</div>
